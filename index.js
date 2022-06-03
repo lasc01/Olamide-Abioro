6b53e2e02b640c5006020453edaff23e6ae79135
lasc01 <103427196+lasc01@users.noreply.github.com>
2022-06-03 15:01:52 +0100

    Add Olamide Abioro website files

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+function myFunc(){
+    var compo = document.querySelector(".nav");
+    if (compo.style.display === "none") {
+        compo.style.display = "block"
+    }
+    else{
+        compo.style.display = "none" 
+    }
+}
